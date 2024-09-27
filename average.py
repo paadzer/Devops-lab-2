1e8c0c37fc3bebe7ae7edd29f02df6213d246d65
Noah <C22535923@mytudublin.ie>
2024-09-27 12:48:46 +0100

    Noahs Update to paddys average file

--- average.py
+++ average.py
@@ -1,4 +1,10 @@
 numbers = input("Enter numbers separated by spaces: ")
 number_list = [float(n) for n in numbers.split()]
+
 average = sum(number_list) / len(number_list)
+smallest = min(number_list)
+largest = max(number_list)
+
 print(f"The average is: {average}")
+print(f"The smallest number is: {smallest}")
+print(f"The largest number is: {largest}")
